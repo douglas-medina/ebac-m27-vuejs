1ff3d94bbbf50e1f2c33fe73ec006e5c79ea6ad7
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-02-18 16:00:53 -0300

    integrando vue js

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const nome = "Douglas Medina"
+const meuObj = {
+  nome: "Douglas",
+  filmeFavorit: "Batman"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const enderecoImagemBatman = "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70070000018073/30648e0a24cff549fe97f586431b4b927a8e49f3531108ec50fd00a106733b4d"
+
+const botaoEstaDesabilitado = true
+
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ dizOla("Ana") }}</h1>
+  <img :src="enderecoImagemBatman" alt="">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button :disabled="botaoEstaDesabilitado">enviar mensagem</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
